2f25323520dc751d4f26f48e3a99497e8835768a
Pete Russell <pinkbatz@gmail.com>
2017-10-11 13:05:00 +1300

    Adds example of setting form fields programmatically with form.setValue() and form.patchValue().

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,36 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    /* setValue() [whole form]
+       patchValue() [partial form]
+
+       There are a few ways we can assign values from here to the form. Two-way
+       data binding would work. There are two others, for entire or partial
+       form changes:
+
+       Method 1 (worse): use setValue(), which needs an exact representation of
+       the entire form. The DOWNSIDE of this method is that the form will be
+       reset to this default set of values every time this suggestUserName()
+       function is called. Because of this, it may be a better idea for form
+       initialization in ngOnInit or similar. All the same, here it is... */
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'female'
+    // });
+
+    /* Method 2 (better): access the field from the signupForm, then use the
+       patchValue() method on the form object contained inside signupForm... */
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
