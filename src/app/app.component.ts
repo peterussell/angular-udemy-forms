5bc0604b64df8a575d2973f6b5bcb3d7c3bba8d6
Pete Russell <pinkbatz@gmail.com>
2017-10-11 13:21:33 +1300

    Adds example of using form data (displaying in template) using template-driven forms and TS class properties.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion: string = 'pet';
   answer: string = '';
   genders: string[] = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -53,6 +61,11 @@ export class AppComponent {
   /* Alternative approach to getting the form data using @ViewChild, which can
      be useful if you need to get access to the form *before* submission. */
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
